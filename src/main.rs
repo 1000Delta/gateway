869980ac4e8936495b4f7b20761589a163776ec6
DeltaX <triangledx@outlook.com>
2024-03-04 02:12:46 +0800

    add pingora lb example

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+mod lb;
+use std::sync::Arc;
+
+use lb::*;
+
+use pingora::prelude::*;
+
+fn main() {
+    let mut my_server = Server::new(None).unwrap();
+    my_server.bootstrap();
+
+    let upstreams = LoadBalancer::try_from_iter(["1.1.1.1:443", "1.0.0.1:443"]).unwrap();
+    let mut lb = http_proxy_service(&my_server.configuration, LB(Arc::new(upstreams)));
+    lb.add_tcp("0.0.0.0:6188");
+
+    my_server.add_service(lb);
+
+    my_server.run_forever();
+}
